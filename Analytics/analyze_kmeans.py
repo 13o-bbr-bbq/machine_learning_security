08ecb7eea2645a1cecafe21370423fb7393d3bee
13o-bbr-bbq <takaesu235@gmail.com>
2017-11-21 23:07:29 +0900

    Add files via upload

---
+++ Analytics/analyze_kmeans.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# Cluster number using k-means.
+CLUSTER_NUM = 5
+
+# Load data.
+df_kddcup = pd.read_csv('.\\dataset\\kddcup.data_small.csv')
+df_kddcup = df_kddcup.iloc[:, [0, 7, 10, 11, 13, 35, 37, 39]]
+
+# Normalization.
+df_kddcup = (df_kddcup - df_kddcup.mean()) / df_kddcup.mean()
+
+# Transpose of matrix.
+kddcup_array = np.array([df_kddcup['duration'].tolist(),
+                      df_kddcup['wrong_fragment'].tolist(),
+                      df_kddcup['num_failed_logins'].tolist(),
+                      df_kddcup['logged_in'].tolist(),
+                      df_kddcup['root_shell'].tolist(),
+                      df_kddcup['dst_host_same_src_port_rate'].tolist(),
+                      df_kddcup['dst_host_serror_rate'].tolist(),
+                      df_kddcup['dst_host_rerror_rate'].tolist(),
+                      ], np.float)
+kddcup_array = kddcup_array.T
+
+# Clustering.
+pred = KMeans(n_clusters=CLUSTER_NUM).fit_predict(kddcup_array)
+df_kddcup['cluster_id'] = pred
+print(df_kddcup)
+print(df_kddcup['cluster_id'].value_counts())
+
+# Visualization using matplotlib.
+cluster_info = pd.DataFrame()
+for i in range(CLUSTER_NUM):
+    cluster_info['cluster' + str(i)] = df_kddcup[df_kddcup['cluster_id'] == i].mean()
+cluster_info = cluster_info.drop('cluster_id')
+kdd_plot = cluster_info.T.plot(kind='bar', stacked=True, title="Mean Value of Clusters")
+kdd_plot.set_xticklabels(kdd_plot.xaxis.get_majorticklabels(), rotation=0)
+
+print('finish!!')
