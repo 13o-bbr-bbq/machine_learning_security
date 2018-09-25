fb019e0ad66cab0a7d337450b9d7c3c2ecc8fbfb
isao takaesu <takaesu235@gmail.com>
2018-09-25 16:19:23 +0900

    Add new code.

---
+++ Security_and_MachineLearning/src/intrusion_detection.py
@@ -0,0 +1,48 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn import metrics
+
+# Load train data
+df_train = pd.read_csv('..\\dataset\\kddcup_train.csv')
+X_train = df_train.iloc[:, [0, 7, 10, 11, 13, 35, 37, 39]]  # feature(X)
+X_train = (X_train - X_train.mean()) / X_train.mean()       # normalization
+y_train = df_train.iloc[:, [41]]                            # label(y)
+
+# Load test data
+df_test = pd.read_csv('..\\dataset\\kddcup_test.csv')
+X_test = df_test.iloc[:, [0, 7, 10, 11, 13, 35, 37, 39]]
+X_test = (X_test - X_test.mean()) / X_test.mean()
+y_test = df_test.iloc[:, [41]]
+
+# We create an instance of Logistic Regression Classifier.
+logreg = linear_model.LogisticRegression(C=1e5)
+
+# Fit (Train)
+start = time.perf_counter()
+logreg.fit(X_train, y_train)
+elapsed_time = time.perf_counter() - start
+print('train_time   : {0}'.format(elapsed_time) + ' [sec]')
+
+# Predict for probability and label
+start = time.perf_counter()
+probs = logreg.predict_proba(X_test)
+elapsed_time = time.perf_counter() - start
+print('predict_time : {0}'.format(elapsed_time) + ' [sec]')
+y_pred = logreg.predict(X_test)
+
+# Evaluation
+print('score : {0}'.format(metrics.accuracy_score(y_test, y_pred)))
+
+# Output predict result
+print('-' * 60)
+print('label\tpredict\tprobability')
+idx = 0
+for predict, prob in zip(y_pred, probs):
+    print('{0}\t{1}\t{2}'.format(y_test.iloc[idx, [0]].values[0], predict, np.max(prob)))
+    idx += 1
+
+print('finish!!')
