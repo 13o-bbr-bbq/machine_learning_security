87058750b8993ef8b8d0b35b06192043fc16d829
isao takaesu <takaesu235@gmail.com>
2019-06-20 17:52:20 +0900

    Fix bugs.

--- Security_and_MachineLearning/src/topic_model.py
+++ Security_and_MachineLearning/src/topic_model.py
@@ -7,9 +7,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
 # Parameters of LDA
-n_samples = 7000
-n_features = 50
-n_topics = 5
+n_samples = 100000
+n_features = 100
+n_topics = 10
 n_top_words = 10
 max_df = 0.95
 min_df = 10
@@ -24,17 +24,22 @@ def print_top_words(model, feature_names):
 
 
 if __name__ == '__main__':
+    full_path = os.path.dirname(os.path.abspath(__file__))
+    root_path = os.path.join(full_path, '..')
+
     # Get original stop words
-    df_stop_words = pd.read_csv('..\\dataset\\topic_model\\stop_words.txt', encoding='utf-8').fillna('')
+    dataset_path = os.path.join(os.path.join(root_path, 'dataset'), 'topic_model')
+    stop_word_path = os.path.join(dataset_path, 'stop_words.txt')
+    df_stop_words = pd.read_csv(stop_word_path, encoding='utf-8').fillna('')
     data_samples = df_stop_words.loc[:].values
     lst_stop_words = [word[0] for word in data_samples.tolist()]
 
     # Load cve description
     lst_cve_description = []
-    for file in os.listdir('..\\dataset\\topic_model\\'):
+    for file in os.listdir(dataset_path):
         # extract description from 'cve****.txt'
-        if file[:3] == 'cve' and file[-4:] == '.txt':
-            df_cve = pd.read_csv('..\\dataset\\topic_model\\' + file, encoding='utf-8').fillna('')
+        if file[:3] == 'cve' and file[-4:] == '.csv':
+            df_cve = pd.read_csv(os.path.join(dataset_path, file), encoding='utf-8').fillna('')
             data_samples = df_cve.loc[random.sample(range(len(df_cve)), n_samples)].values
             lst_cve_description = [description[0] for description in data_samples.tolist()]
         else:
